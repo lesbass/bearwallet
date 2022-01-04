81723e7ae8f40fb1fd4e2be46ff3a4d306f2c883
Stefano Maffeis <stefano.maffeis@casavo.com>
2022-01-04 16:15:44 +0100

    lazy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 
 import useAuthentication from 'hooks/useAuthentication'
 
-import AuthenticatedApp from './AuthenticatedApp'
 import AppLoader from './loader'
-import LoginForm from './login'
+
+const AuthenticatedApp = lazy(() => import('./AuthenticatedApp'))
+const LoginForm = lazy(() => import('./login'))
 
 const App: React.VFC = () => {
   const { user } = useAuthentication()
 
   switch (user?.isLoggedIn) {
     case true:
-      return <AuthenticatedApp />
+      return <Suspense fallback={<AppLoader />}><AuthenticatedApp /></Suspense>
     case false:
-      return <LoginForm />
+      return <Suspense fallback={<AppLoader />}><LoginForm /></Suspense>
     default:
       return <AppLoader />
   }
